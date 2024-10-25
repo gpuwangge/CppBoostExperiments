b5973655e7046b860510549ebcee4cdd1668d5a5
王哥流浪天涯 <wxj995@gmail.com>
2024-10-25 09:47:11 -0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include "../include/main.h"
+#include <boost/program_options.hpp>
 
-int main(){
-	std::cout<<"hello"<<std::endl;
+namespace po = boost::program_options;
+int main(int ac, char* av[]){
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+        ("compression", po::value<int>(), "set compression level")
+    ;
+    
+    po::variables_map vm;
+    po::store(po::parse_command_line(ac, av, desc), vm);
+    po::notify(vm);    
+    
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return 1;
+    }
+    
+    if (vm.count("compression")) {
+        std::cout << "Compression level was set to " << vm["compression"].as<int>() << ".\n";
+    } else {
+        std::cout << "Compression level was not set.\n";
+    }
 
-    return 0;
 }
